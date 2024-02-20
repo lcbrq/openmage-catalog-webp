bc132a9fd3f775a5ca786395df6e6c45bf098ace
Tomasz Gregorczyk <tomasz@silpion.com.pl>
2024-02-20 16:19:03 +0100

    Separate storage folder and flush webp images on product save

--- app/code/local/LCB/CatalogWebP/Helper/Data.php
+++ app/code/local/LCB/CatalogWebP/Helper/Data.php
@@ -3,29 +3,73 @@
 class LCB_CatalogWebp_Helper_Data
 {
     /**
+     * @var string
+     */
+    private $storagePrefix = 'webp';
+
+    /**
      * @param  string $image
      * @return string
      */
-    public function convert(string $image): string
+    public function convert($image): string
     {
         $pathInfo = pathinfo($image);
         if (strtolower($pathInfo['extension']) !== 'webp') {
             $imageFolder = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'product';
+            $webFolder = $this->getWebFolder();
             $webpImage = $pathInfo['dirname'] . DS . $pathInfo['filename'] . '.webp';
-            $webpPath = $imageFolder . $webpImage;
+            $webpDir = $webFolder . $pathInfo['dirname'];
+            $webpPath = $webFolder . $webpImage;
             if (file_exists($webpPath)) {
-                return $webpImage;
+                return $this->storagePrefix . $webpImage;
             }
 
             $imagePath = $imageFolder . $image;
             if (file_exists($imagePath)) {
-                exec("cwebp $imagePath -o $webpPath 2>&1", $output, $exitCode);
-                if (!$exitCode) {
-                    return $webpImage;
+                if (!file_exists($webpPath) && @mkdir($webpDir, 0777, true)) {
+                    exec("cwebp $imagePath -o $webpPath 2>&1", $output, $exitCode);
+                    if (!$exitCode) {
+                        return $this->storagePrefix . $webpImage;
+                    }
                 }
             }
         }
 
         return $image;
+    }
+
+    /**
+     * @return void
+     */
+    public function refreshImages()
+    {
+        foreach ([$product->getImage(), $product->getSmallImage()] as $image) {
+            if ($image === 'no_selection') {
+                continue;
+            }
+            $pathInfo = pathinfo($image);
+            if (strtolower($pathInfo['extension']) === 'webp') {
+                $webpImage = $pathInfo['dirname'] . DS . $pathInfo['filename'] . '.webp';
+                $webpPath = $this->getWebFolder() . $webpImage;
+                unlink($webpPath);
+            }
+        }
+    }
+
+    /**
+     * @param  string $image
+     * @return string
+     */
+    public function getWebPath($image)
+    {
+        $pathInfo = pathinfo($image);
+    }
+
+    /**
+     * @return string
+     */
+    public function getWebFolder()
+    {
+        return Mage::getBaseDir('media') . DS . 'catalog' . DS . 'product' . DS . $this->storagePrefix;
     }
 }
--- app/code/local/LCB/CatalogWebP/Model/Observer.php
+++ app/code/local/LCB/CatalogWebP/Model/Observer.php
@@ -3,17 +3,31 @@
 class LCB_CatalogWebP_Model_Observer
 {
     /**
+     * @var bool
+     */
+    private $isEnabled;
+
+    public function __construct()
+    {
+        $this->isEnabled = Mage::helper('core')->isModuleEnabled('LCB_CatalogWebP');
+    }
+
+    /**
      * @param  Varien_Event_Observer $observer
      * @return void
      */
     public function onCatalogProductLoadAfter(Varien_Event_Observer $observer): void
     {
         $product = $observer->getProduct();
-        if ($product) {
+        if ($this->isEnabled && $product) {
             $helper = Mage::helper('lcb_catalogwebp');
             $image = $product->getImage();
             if ($image && $image !== 'no_selection' && strtolower(substr($image, -4)) !== 'webp') {
-                $product->setImage($helper->convert($image));
+                try {
+                    $product->setImage($helper->convert($image));
+                } catch (Exception $e) {
+                    Mage::logException($e);
+                }
             }
         }
     }
@@ -26,12 +40,32 @@ class LCB_CatalogWebP_Model_Observer
     {
         $collection = $observer->getCollection();
         foreach ($collection as $product) {
-            if ($product) {
+            if ($this->isEnabled && $product) {
                 $helper = Mage::helper('lcb_catalogwebp');
                 $image = $product->getSmallImage();
                 if ($image && $image !== 'no_selection' && strtolower(substr($image, -4)) !== 'webp') {
-                    $product->setSmallImage($helper->convert($image));
+                    try {
+                        $product->setSmallImage($helper->convert($image));
+                    } catch (Exception $e) {
+                        Mage::logException($e);
+                    }
                 }
+            }
+        }
+    }
+
+    /**
+     * @param  Varien_Event_Observer $observer
+     * @return void
+     */
+    public function onCatalogProductSaveAfter(Varien_Event_Observer $observer): void
+    {
+        $product = $observer->getProduct();
+        if ($this->isEnabled && $product) {
+            try {
+                $webPath = Mage::helper('lcb_catalogwebp')->refreshImages($product);
+            } catch (Exception $e) {
+                Mage::logException($e);
             }
         }
     }
